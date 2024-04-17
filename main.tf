fb78e163fabb148a013f8810128a57fc754ac50f
mohammad9haseeb <mohammad9haseeb@gmail.com>
2024-04-17 16:44:08 -0400

    #learning ansible# ssm

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
   name  = "test.test"
-  type  = "String"
+  type  = "SecureString"
   value = "helloworld"
+  key_id = "4d59713b-3f51-4512-9a87-148078194b87"
 }
